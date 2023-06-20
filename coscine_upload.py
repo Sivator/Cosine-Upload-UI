d179b6cbae0638b458003c96db30302e5ccbcc3e
Sivator <39954155+Sivator@users.noreply.github.com>
2023-06-20 08:06:19 +0200

    Changed the naming of entry inside cosince
    Instead of the title the name of the file gets display inside coscine. The same way it would apear if you upload something using the website.

--- coscine_upload.py
+++ coscine_upload.py
@@ -1,10 +1,11 @@
 import coscine
 from datetime import datetime
 import tkinter as tk
+import os
 
 #Fills the metadata form and uploads the file to coscine
 def upload_coscine(token, path, project_name, resource_name, m_title,m_name,m_date,m_supervisor,m_reviewer,m_course, m_project, m_location, m_institute, m_degree, m_archivist,m_groups, m_organism, m_molecules, m_methodology, m_partners,m_abstract):
-    
+
     #Connect to coscine
     client = coscine.Client(token)
     form = client.project_form()
@@ -31,6 +32,6 @@ def upload_coscine(token, path, project_name, resource_name, m_title,m_name,m_da
     metadata["Methodology"] = m_methodology
     metadata["Project partners"] = m_partners
     metadata["Abstract"] = m_abstract
-
-    filename = m_title + ".pdf"
-    resource.upload(filename, path, metadata)
+    
+    file_name = os.path.split(path)[1]
+    resource.upload(file_name, path, metadata)
